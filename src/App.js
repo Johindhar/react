802c9a9a664ae8a0ab6180751b31395128548dba
Johindhar <johinaveen10@gmail.com>
2022-11-17 10:50:53 +0530

    2nd commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
 
 import './App.css';
 import Employee from './components/Employee';
+import {useState} from 'react';
 
 function App() {
+  const [role, setRole] = useState('dev');
+  const showEmployees = true;
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-         <Employee />
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       { showEmployees ?(
+        <>  
+        <input 
+        type='text' 
+        onChange={(e) => {
+          console.log(e.target.value);
+          setRole(e.target.value);
+        }} />
+            <Employee name="Caleb" role="Intern"/>
+            <Employee name = "Abby" role = {role}/>
+            <Employee name = "John"/>
+        </>
+       ):(
+          <p>you cannot see employee</p>
+
+       )}       
     </div>
   );
 }
--- src/components/Employee.js
+++ src/components/Employee.js
@@ -1,5 +1,11 @@
-function Employee(){
-    return <h3>this is an employee</h3>
+function Employee(props){
+    return (
+        <>
+        <h3>Employee {props.name}</h3>
+        <p>{props.role ? props.role : "No Role"}</p>
+            {/* {props.role ? <p class='role'>{props.role}</p> : <p class='norole'>No Role</p>} */}
+        </>
+    )
 }
 
 export default Employee;
